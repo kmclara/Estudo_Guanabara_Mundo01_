a47f786e4ef989862443bc47d6f9038184253964
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-08-23 15:20:02 -0300

    erro novamente para rodar o mp3. jeito do guanabara não deu

--- cursoemvideo08/exercicioaula08-21.py
+++ cursoemvideo08/exercicioaula08-21.py
@@ -3,8 +3,6 @@
 import pygame
 
 pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load("C:\Users\maria\OneDrive\Área de Trabalho\deuseeu.mp3")
+pygame.mixer.music.load("deuseeu.mp3")
 pygame.mixer.music.play()
 pygame.event.wait()
-input()
