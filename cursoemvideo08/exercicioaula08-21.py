96ed358955d55f3804e27cced814a8eb0a36979e
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-08-23 16:23:47 -0300

    códigos mudados de acordo com explicação dos comentários do vídeo do guanabara

--- cursoemvideo08/exercicioaula08-21.py
+++ cursoemvideo08/exercicioaula08-21.py
@@ -1,8 +1,8 @@
 #Importando mp3 no PYTHON. 
 
-import pygame
+from pygame import mixer 
 
-pygame.init()
-pygame.mixer.music.load("deuseeu.mp3.mp3")
-pygame.mixer.music.play()
-pygame.event.wait()
+mixer.init()
+mixer.music.load("deuseeu.mp3")
+mixer.music.play()
+input("Agora voce escuta?")
