84a239d06dd2f59be3934f4051b2486b03036b40
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-08-24 15:28:20 -0300

    DESAFIO 24:
    No nome da cidade posta, consta a palavra Santos?
    Usa-se o operador in.
    Na linha 3 consta a variavel city para colocar o nome da cidade.
    Linha 7, onde está o operador In.
    Se na cidade houver o nome Santos, retornará true, se não tiver, retornará false.
    Na linha 11 consta o print para o usuário ler o resultado.

---
+++ cursoemvideo09/desafio24-aula009.py
@@ -0,0 +1,11 @@
+#DESAFIO 24
+
+city = str(input("Me indique o nome da sua cidade: "))
+
+#indicar se tem o nome santo ou não na cidade:
+
+santo = "Santo" in city 
+
+#escrevendo o resultado: 
+
+print("O nome Santo é {} no nome da sua cidade.".format(santo))
