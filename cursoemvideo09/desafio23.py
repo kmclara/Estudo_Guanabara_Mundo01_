57477805698543184e600a7212d453b6786ec3d9
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-08-24 15:15:14 -0300

    DESAFIO 23
    Achar o valor de unidade, dezena, centena e milhar de um número.
    Na linha 3 e 4 foram postas duas variaveis. Sendo a três para o informativo do número posto pelo usuário e a segunda para informar o número posto nos códigos em diante, para uma maior facilidade.
    Linha 6, frase informativa ao usuário.
    Linha 7, 8, 9 e 10 contém os resultados propostos usando o fatiamento, dado em .format(n[2]), por exemplo.

---
+++ cursoemvideo09/desafio23.py
@@ -0,0 +1,10 @@
+#DESAFIO 23
+
+num = int(input("Digite um número:"))
+n = str(num)
+
+print("De acordo com o número posto, {}".format(num))
+print("Unidade: {}".format(n[3]))
+print("Dezena: {}".format(n[2]))
+print("Centena: {}".format(n[1]))
+print("Milhar: {}".format(n[0]))
