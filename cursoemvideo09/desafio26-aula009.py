8903d39199ec5f4f9e775ee6526a7505f36dc16b
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-08-30 14:21:41 -0300

    DESAFIO 26: O desafio consistia em: Quantas vezes aparece a letra A na str, em que posição ela aparece na primeira vez e na ultima vez. Foram usadas frase.count() para quantas vezes ela aparece, find() para posição que ela aparece primeiro e rfind() para a posição que ela aparece por ultimo. Usadas tambem na variavel strip() para tirar espaços inuteis que o usuario pode colocar e lower() para formatar tudo em minusculo e não ter perigo de leitura errada pela maquina +1 foi colocado para mostrar a posição que ela está de acordo com a nossa contagem, já que o pc conta apartir do 0.

--- cursoemvideo09/desafio26-aula009.py
+++ cursoemvideo09/desafio26-aula009.py
@@ -1,1 +1,28 @@
 #DESAFIO 009
+
+frase = str(input("Adicione uma frase: ")).strip().lower()
+
+#Quantas vezes aparece a letra A na frase?
+
+A = frase.count("a")
+
+#Em que posição a letra A aparece pela primeira vez?
+
+prim = frase.find("a")+1
+
+#Em que posição ela aparece a última vez
+
+ult = frase.rfind("a")+1
+
+print("Quantas vezes aparece {}, primeira {}, ultima {}".format(A, prim, ult))
+
+#Apenas algumas coisas que mudei:
+#- rfind: não sabia que poderia usar assim, então
+#foi uma das coisas que mudei. rfind faz com que 
+#comece a busca do lado direito e adicionei +1 
+# - usei apenas find no prim, não sabia que ele
+#poderia buscar já a primeira posição da str e
+#adicionei +1
+# - coloquei strip para retirar os espaços indesejados
+#que algum usuário pode vir a fazer. e coloquei 
+#lower() junto com a variavel frase. 
