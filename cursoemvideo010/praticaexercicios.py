87312097128d930b19a5e361cdb1b61927f6e987
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-01 14:32:04 -0300

    Estrutura composta em um programinha de calculo de notas. Linha 18 if n >=6: (print o que deve voltar) Linha else caso não ser o que if pediu.

--- cursoemvideo010/praticaexercicios.py
+++ cursoemvideo010/praticaexercicios.py
@@ -1,9 +1,22 @@
 #Práticando a teoria. 
 
-nome = str(input("Diga seu nome: "))
+#nome = str(input("Diga seu nome: "))
 
-if nome == "Maria":
-   print("Seu nome é lindo")
-else: 
-    print("Seu nome é feio!")
-print("Boa tarde!")
+#if nome == "Maria":
+   #print("Seu nome é lindo")
+#else: 
+    #print("Seu nome é feio!")
+#print("Boa tarde!")
+
+n1 = float(input("Adicione sua nota: "))
+n2 = float(input("Adicione a outra nota: "))
+
+n = (n1+n2)/2
+
+print("A sua média de notas foi de {:.1f}!".format(n))
+
+if n >= 6.0:
+    print("Sua média está boa! Parabéns!")
+else:
+    print("Sua média não está boa! Estude mais na próxima!")
+    
