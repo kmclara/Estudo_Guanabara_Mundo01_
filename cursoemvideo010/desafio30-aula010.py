b1065494c66bd19612b2b3ff2cd18f5a257ce5dd
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-02 14:29:00 -0300

    desafio 30, par ou impar, usado resto de uma divisão na linha 7

---
+++ cursoemvideo010/desafio30-aula010.py
@@ -0,0 +1,16 @@
+#DESAFIO 30
+
+#Perguntando ao usuário um número:
+number = float(input("Digite um número: "))
+
+#Se?
+if (number%2) == 0:
+    print("Seu número é par!")
+else:
+    print("Seu número é impar!")
+print("Adicione outro novamente para ser avaliado!")
+
+#Para saber se um número é par ou impar, basta 
+#dividir ele por 2. Se for par, o resto é sempre
+#0, não sobra nada. 
+#Já se for impar, vai sempre ter resto 1. 
