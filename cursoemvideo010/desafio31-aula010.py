b3469c12359d701f6f1e37940aee4948a204d910
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-02 14:51:32 -0300

    trocando 0,50 e 0,45 por 0.50 e 0.45, pois deu erro o número decial 0,50 com vírgula

--- cursoemvideo010/desafio31-aula010.py
+++ cursoemvideo010/desafio31-aula010.py
@@ -1,1 +1,15 @@
 #DESAFIO 31
+
+#Perguntando a distância da viagem em km:
+km = float(input("Qual é a distância da viagem? "))
+
+#Calculando a passagem:
+cal1 = km*0.50
+cal2 = km*0.45
+
+#Se?
+if km <= 200:
+    print("Você irá pagar {} de passagem!".format(cal1))
+else:
+    print("Você irá pagar {} de passagem!".format(cal2))
+print("Boa viagem!")
