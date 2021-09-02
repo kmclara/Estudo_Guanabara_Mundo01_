67b5bd58f0261fe55f8ab6736cbb6f51256f3089
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-02 15:25:18 -0300

    add import datetime para reconhecer o ano atual do pc

--- cursoemvideo010/desafio32-aula010.py
+++ cursoemvideo010/desafio32-aula010.py
@@ -1,9 +1,14 @@
 #DESAFIO 32
 
+#Importando para saber a data do pc:
+from datetime import date 
+
 #Perguntando o ano para o usuário: 
-ano = int(input("Adicione o ano que deseja: "))
+ano = int(input("Adicione o ano que deseja! Se quiser analisar o ano atual, digite 1! "))
 
 #Se?
+if ano == 1:
+    ano = date.today().year 
 if (ano%4==0 and ano%100!=0) or (ano%400==0):
     print("O ano {} é bissexto!".format(ano))
 else:
