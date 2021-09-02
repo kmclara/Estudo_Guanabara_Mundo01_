86dc9ebbc941a9a458923c74d81153a774b3b314
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-02 15:21:21 -0300

    add comentário sobre !=

--- cursoemvideo010/desafio32-aula010.py
+++ cursoemvideo010/desafio32-aula010.py
@@ -1,2 +1,13 @@
 #DESAFIO 32
 
+#Perguntando o ano para o usuário: 
+ano = int(input("Adicione o ano que deseja: "))
+
+#Se?
+if (ano%4==0 and ano%100!=0) or (ano%400==0):
+    print("O ano {} é bissexto!".format(ano))
+else:
+    print("O ano {} não é bissexto!".format(ano))
+print("Obrigada por usar nosso programa!")
+
+#!= diferente, não igual. 
