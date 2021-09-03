7e1d412bcd54d092449e62ca01463f7e5be2eb77
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-03 14:32:59 -0300

    programa terminado, desafio 35

--- cursoemvideo010/desafio35-aula010.py
+++ cursoemvideo010/desafio35-aula010.py
@@ -6,4 +6,13 @@ b = float(input("Me diga a segunda reta: "))
 c = float(input("Me diga a terceira reta: "))
 
 #Cálculo: 
-cal1 = ()
+cal1 = (a+b)
+cal2 = (b+c)
+cal3 = (a+c)
+
+#Se
+if cal1 > c and cal2 > a and cal3 > b:
+    print("Suas retas podem formar um triângulo!")
+else: 
+    print("Suas retas não podem formar um triângulo!")
+print("Obrigada por usar nosso programa!")
