2e3a48b30a9f168fef2778c1598f05e10124c24b
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-03 14:12:33 -0300

    desafio 34 finalizado, usado if e else junto com as somas padrão para o cálculo do aumento do salário.

--- cursoemvideo010/desafio34-aula010.py
+++ cursoemvideo010/desafio34-aula010.py
@@ -1,3 +1,15 @@
 #Desafio 34 
 
-#
+#Perguntando o salário:
+salário = float(input("Digite seu salário para o cálculo do aumento: "))
+
+#Cálculo dos aumentos:
+aumento1 = (0.10+1)*salário 
+aumento2 = (0.15+1)*salário 
+
+#Se?
+if salário >=1250.00:
+    print("O valor total do seu aumento é: R${:.2f}!".format(aumento1))
+else:
+    print("O valor do seu salário total é: R${:.2f}!".format(aumento2))
+print("Obrigada por usar nosso programa!")
