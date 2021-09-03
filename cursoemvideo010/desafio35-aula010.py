b009094d02dc71c650e5c868a829a7fbf23dbdb0
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-03 14:18:40 -0300

    adicionando as váriaveis para ser adicionado os valores das retas pelo usuário

--- cursoemvideo010/desafio35-aula010.py
+++ cursoemvideo010/desafio35-aula010.py
@@ -1,2 +1,6 @@
 #DESAFIO 35
 
+#Pedindo ao usuário três retas:
+r1 = float(input("Me diga a primeira reta: "))
+r2 = float(input("Me diga a segunda reta: "))
+r3 = float(input("Me diga a terceira reta: "))
