7a3ff17d5500f2b4c4fc1b113e8e507353663958
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-06 15:14:42 -0300

    I removed elif from line 9 and added another elif with another command

--- Mundo02-Aula12-Python.py/práticaaula12.py
+++ Mundo02-Aula12-Python.py/práticaaula12.py
@@ -1,1 +1,14 @@
 #Prática mundo 02 aula 12. 
+
+#Perguntando nome ao usuário. 
+nome = str(input("Qual é seu nome? "))
+
+#If, elif and else. 
+if nome == "Maria": 
+    print("Que nome bonito! Parabéns!")
+elif nome == "Pedro" or nome == "Camila":
+    print("Está quase lá! Seu nome é mediano!")   
+else:
+    print("Seu nome não é tão bonito assim! Que pena!")
+
+print("Tenha um bom dia!")
