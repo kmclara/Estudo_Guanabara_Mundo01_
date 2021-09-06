c41165af0b490dc6ab66d666a3a519416716bc7a
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-06 16:35:05 -0300

    added calculation in line 11, removed elif and changed line 14 (if)
    linha 11: calculo excedeu adicionado
    linha 14 modificado, colocado > entre mensal e excedeu.
    trad: adicionei cálculo na linha 11, retirei elif e mudei linha 14 (if).

--- Mundo02-Aula12-Python.py/desafio36aula12-mundo02.py
+++ Mundo02-Aula12-Python.py/desafio36aula12-mundo02.py
@@ -1,1 +1,19 @@
 #DESAFIO 36
+
+#Perguntando o salário, valor da casa e quantos anos vai morar na casa: 
+salário = float(input("Qual seu salário? "))
+casa = float(input("Qual o valor da casa: "))
+anos = int(input("Quantos anos pretende morar na casa? "))
+
+#Cálculo prestação mensal e porcentagem do exceder de 30% 
+mensal = casa / (anos*12)
+print("O valor do aluguel mensal que você terá que pagar é de {:.2f}!".format(mensal))
+excedeu = salário * 0.30
+
+#Se? Senão se? Senão?
+if mensal > excedeu:
+    print("Você não poderá pagar este aluguel! O valor excedeu 30% do seu salário!")
+else: 
+    print("Você poderá pagar o aluguel! O valor está logo acima!")
+
+print("Obrigada por usar nosso programa!")
