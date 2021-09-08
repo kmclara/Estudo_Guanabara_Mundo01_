a727d531660021781f50af290bb8634a761a9fa9
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-08 16:24:00 -0300

    Mudado a ordem de subtração linha 16
    antes: idade - idadecorreta (variaveis)
    agora: idadecorreta - idade
    Foi feita essa mudança pois o valor atribuido a variavel idade era menor que o valor atribuido a variavel idadecorreta, sendo assim, o resultado estava dando negativo.

--- Mundo02-Aula12-Python.py/desafio39aula12-mundo02.py
+++ Mundo02-Aula12-Python.py/desafio39aula12-mundo02.py
@@ -1,1 +1,26 @@
 #DESAFIO 39
+
+#Importando o ano do PC:
+from datetime import date 
+
+#Perguntando o ano de nascimento do usuário:
+nascimento = int(input("Qual o ano do seu nascimento? "))
+
+#Fazendo a função do datetime:
+anocomputador = date.today().year
+
+#Cálculo para saber a idade do usuário:
+idade = (anocomputador-nascimento)
+#Cálculo para mostrar quanto tempo falta ou se passou do ponto:
+idadecorreta = 18 
+falta = (idadecorreta - idade)
+
+#Se, se senão, senão?
+if idade < 18:
+    print("Você ainda não precisa se alistar! Falta {} anos".format(falta))
+elif idade == 18:
+    print("Já é hora de se alistra no exercito militar!")
+else: 
+    ("Você precisa se alistar! Já se Passou o tempo de alistamento de acordo com sua idade!")
+
+print("Obrigada por usar nosso programa!")
