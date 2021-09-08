41f723e2b9ff4de73977a8c1c3d8baa5cb8252e3
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-08 15:05:22 -0300

    Modificação linha 16
    Linha 16 estava elif numero>numero1
    Mudado para elif numero2<numero1
    Correção primeira linha acima:
    Linha 16 estava elif numero2>numero1

--- Mundo02-Aula12-Python.py/desafio38aula12-mundo02.py
+++ Mundo02-Aula12-Python.py/desafio38aula12-mundo02.py
@@ -1,1 +1,25 @@
 #Desafio 38
+
+#Pedindo ao usuário dos números inteiros:
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+#Verificando qual é maior, menor ou igual:
+#MAIOR:
+if numero1>numero2: 
+    print("O número {} é maior que o valor {}!".format(numero1, numero2))
+elif numero2>numero1:
+    print("O valor {} é maior que o valor {}!".format(numero2, numero1))
+#MENOR:
+if numero1<numero2: 
+    print("O valor {} é menor que o valor {}!".format(numero1, numero2))
+elif numero2<numero1: 
+    print("O valor {} é menor que o valor {}!".format(numero2, numero1))
+#IGUAL
+if numero1 == numero2:
+    print("O valor {} e o valor {} são iguais!".format(numero1, numero2))
+elif numero2 == numero1:
+    print("O valor {} é igual ao valor {}!".format(numero2, numero1))
+
+print("Obrigada por usar nosso programa!")
+
