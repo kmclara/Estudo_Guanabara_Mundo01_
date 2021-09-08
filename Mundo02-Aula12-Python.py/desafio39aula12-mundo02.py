aa057e3eaf34cc2aa47cb007a39e90ce24ab5efc
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-08 20:03:08 -0300

    Novo nome as variaveis
    Usando estilo de codigo Camel Case

--- Mundo02-Aula12-Python.py/desafio39aula12-mundo02.py
+++ Mundo02-Aula12-Python.py/desafio39aula12-mundo02.py
@@ -7,23 +7,23 @@ from datetime import date
 nascimento = int(input("Qual o ano do seu nascimento? "))
 
 #Fazendo a função do datetime:
-anocomputador = date.today().year
+anoComputador = date.today().year
 
 #Cálculo para saber a idade do usuário:
-idade = (anocomputador-nascimento)
+idade = (anoComputador-nascimento)
 #Cálculo para mostrar quanto tempo falta ou se passou do ponto:
-idadecorreta = 18 
+idadeCorreta = 18 
 #falta = (idadecorreta - idade)
 #passou = (idade - idadecorreta)
 
 #Se, se senão, senão?
 if idade < 18:
-    falta = (idadecorreta - idade)
+    falta = (idadeCorreta - idade)
     print("Você ainda não precisa se alistar! Falta {} anos".format(falta))
 elif idade == 18:
     print("Já é hora de se alistra no exercito militar!")
 else: 
-    passou = (idade - idadecorreta)
+    passou = (idade - idadeCorreta)
     print("Você precisa se alistar! Já se Passou o tempo de alistamento de acordo com sua idade! \n Se passou {} anos!".format(passou))
 
 print("Obrigada por usar nosso programa!")
