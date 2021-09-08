ec53c2842538506cb8ae13a6bae62662d11b6069
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-08 14:32:11 -0300

    mudança print linha 22
    Linha 22: Antes: Valor invalido.
    Linha 22: Agora: Opçao Invalida

--- Mundo02-Aula12-Python.py/desafio37aula12-mundo02.py
+++ Mundo02-Aula12-Python.py/desafio37aula12-mundo02.py
@@ -1,1 +1,25 @@
 #DESAFIO 37
+
+#Perguntando ao usuário um número para a conversão:
+num = int(input("Digite um número inteiro: "))
+
+#Pedindo ao usuário que escolha uma opção:
+print(""" Digite a opção de conversão:
+[1] BINÁRIO
+[2] OCTAL 
+[3] HEXADECIMAL """)
+
+opção = int(input("Sua opção: "))
+
+#Se, Se senão, Senão?
+if opção == 1: 
+    print("{} convertido para binário será {}!".format(num, bin(num)[2:]))
+elif opção == 2: 
+    print("{} convertido para octal será {}!".format(num, oct(num)[2:]))
+elif opção == 3: 
+    print("{} convertido para hexadecimal é {}!".format(num, hex(num)[2:]))
+else: 
+    print("Opção invalida. Tente novamente!")
+
+#Final
+print("Obrigada por usar nosso programa!")
