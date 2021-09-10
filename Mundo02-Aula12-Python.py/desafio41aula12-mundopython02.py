235a04b443b71e1aa603873c51f5c2eea006ff98
Maria Clara B <mariaclarabritosantana@gmail.com>
2021-09-10 14:03:45 -0300

    Adicionado print
    Linha 12

--- Mundo02-Aula12-Python.py/desafio41aula12-mundopython02.py
+++ Mundo02-Aula12-Python.py/desafio41aula12-mundopython02.py
@@ -1,1 +1,26 @@
 #Desafio 41
+
+#Importando datetime
+from datetime import date
+dataComputador = date.today().year
+
+#Perguntando ao usuário seu ano de nascimento: 
+dataNascimento = int(input("Informe sua data de nascimento: "))
+
+#Cálculo para saber a idade do atleta:
+idadeAtleta = (dataComputador - dataNascimento)
+print("Sua idade é {}. De acordo com ela, sua categoria está logo abaixo!".format(idadeAtleta))
+
+#Se, se senão, senão?
+if idadeAtleta <=9:
+    print("Sua categoria é a MIRIM! Bons jogos!")
+elif idadeAtleta >=10 and idadeAtleta <=14:
+    print("Sua categoria é INFANTIL! Bons jogos!")
+elif idadeAtleta >=15 and idadeAtleta <=19:
+    print("Sua categoria é JUNIOR! Bons jogos!")
+elif idadeAtleta == 20:
+    print("Sua categoria é SÊNIOR! Bons jogos!")
+else:
+    print("Sua categoria é MASTER! Bons jogos!")
+
+print("Obrigada por usar nosso programa!")
